a10a50fd3da367b4bc627a5db2ee935b1e1eaf18
paul <pdjstone@users.noreply.github.com>
2024-12-10 17:41:29 +0000

    more minimal

--- 10-min.py
+++ 10-min.py
@@ -1,11 +1,11 @@
 b=[*open('10')]
 w=len(b)
 b=[*map(int,''.join(l[:-1]for l in b))]
-n=[1,0,-1,0]
+n=1,0,-1,0
 def t(p):
  h={};v=b[p]
  if v>8:return{p:1}
- for d in(0,1,2,3):
+ for d in 0,1,2,3:
   x=p%w+n[d];y=p//w+n[-d-1];P=y*w+x
   if(0<=x<w)&(0<=y<w)and b[P]==v+1:h|=t(P)
  return h
