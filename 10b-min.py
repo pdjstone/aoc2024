d36140aff293ff12a1b24d5b783f0d3152212172
paul <pdjstone@users.noreply.github.com>
2024-12-10 17:34:53 +0000

    Minimize day 10

---
+++ 10b-min.py
@@ -0,0 +1,12 @@
+b=[*open('10')]
+w=len(b)
+b=[*map(int,''.join(l[:-1]for l in b))]
+n=[1,0,-1,0]
+def t(p):
+ h=0;v=b[p]
+ if v>8:return 1
+ for d in(0,1,2,3):
+  x=p%w+n[d];y=p//w+n[-d-1];P=y*w+x
+  if(0<=x<w)&(0<=y<w)and b[P]==v+1:h+=t(P)
+ return h
+print(sum(t(p)for p,v in enumerate(b)if v<1))
