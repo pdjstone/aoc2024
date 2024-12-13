c279b42780a025d242f4f61b7dff8df8658709de
paul <pdjstone@users.noreply.github.com>
2024-12-13 15:31:39 +0000

    non-minified version

--- 13.py
+++ 13.py
@@ -1,3 +1,14 @@
-import re,z3
-def s(p):h,j,k,l,x,y=map(int,re.findall('\d+',p));O=10**13;A,B=z3.Ints('A B');s=z3.Solver();s+=A*h+B*k==x+O,A*j+B*l==y+O;return int(str(s.model().eval(A*3+B)))if s.check()==z3.sat else 0
-print(sum(map(s,open('13').read().split('\n\n'))))
+import re
+from z3 import Solver, Ints, unsat
+
+def solve(p):
+    ax, ay, bx, by, px, py = map(int, re.findall('\d+', p))
+    O = 10000000000000
+    A, B = Ints('A B')
+    s = Solver()
+    s += A*ax + B*bx == px + O, A*ay + B*by == py + O
+    if s.check() == unsat:
+        return 0
+    return s.model().eval(A*3+B).as_long()
+
+print(sum(map(solve, open('13').read().split('\n\n'))))
