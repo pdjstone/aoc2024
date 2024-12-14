e73aec4d079774d37061d23a7fb9088e4c822672
Paul Stone <paul@pdjs.co.uk>
2024-12-14 18:47:14 +0000

    simpler

--- 14.py
+++ 14.py
@@ -11,7 +11,7 @@ for l in open('14'):
   vy=(h+vy)%h
   x=(x+vx*i)%w
   y=(y+vy*i)%h
-  if x==mw or y==mh: continue
-  s[[0,1][x>mw]+[0,2][y>mh]]+=1
+  if x!=mw and y!=mh: 
+   s[[0,1][x>mw]+[0,2][y>mh]]+=1
 a,b,c,d=s
 print(a*b*c*d)
